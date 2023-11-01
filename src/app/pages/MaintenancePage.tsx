9b4d4e2fddd0b4611a47ae1a7324e9e2fdb5a6d3
Pavel Kilian <pavel.k.i.pp@hotmail.com>
2023-11-01 14:47:48 +0100

    Structure fix

--- src/app/pages/MaintenancePage.tsx
+++ src/app/pages/MaintenancePage.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const MaintenancePage = () => (
   <>
@@ -7,13 +7,19 @@ const MaintenancePage = () => (
     </Head>
     <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center">
       <div className="container mx-auto p-4 bg-gray-800 shadow-lg rounded-lg text-white text-center">
-        <h1 className="text-4xl font-semibold text-gray-100 mb-4">Site Under Maintenance</h1>
+        <h1 className="text-4xl font-semibold text-gray-100 mb-4">
+          Site Under Maintenance
+        </h1>
         <p className="text-lg mb-4">
-          Sorry, this site is currently under maintenance. Please come back later.
+          Sorry, this site is currently under maintenance. Please come back
+          later.
           <br />
           <br />
-          You can get in touch with me by mail :{' '}
-          <a href="mailto:Pavel.k.i@hotmail.com" className="text-blue-400 hover:underline">
+          You can get in touch with me by mail :{" "}
+          <a
+            href="mailto:Pavel.k.i@hotmail.com"
+            className="text-blue-400 hover:underline"
+          >
             <b>Pavel.k.i@hotmail.com</b>
           </a>
         </p>
