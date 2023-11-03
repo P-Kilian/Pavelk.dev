3319e252f743db5632076d4f710091220e8dc80e
P-Kilian <122708894+P-Kilian@users.noreply.github.com>
2023-11-03 06:18:31 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="px-[400px]">{children}</div>
+        <div className="">{children}</div>
       </body>
     </html>
   );
